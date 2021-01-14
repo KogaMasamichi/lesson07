ce6b305df18863fdffb55a734365417cd4ce0f3a
KogaMasamichi <km.060928@gmail.com>
2021-01-15 01:12:21 +0900

    ex08-1完成

---
+++ ex08/ex08-1.js
@@ -0,0 +1,9 @@
+const link = document.querySelector("a")
+console.log(link.textContent)
+
+const todo = document.getElementById("todo")
+const title = todo.querySelector("h1")
+console.log(title.textContent)
+
+const list = todo.querySelector(".list")
+console.log(list.children[1].textContent)
